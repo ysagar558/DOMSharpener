4fd424635fb973f7168023ab4209c93e57fca649
ysagar558 <ysagar558@gmail.com>
2025-08-26 16:36:32 +0530

    initial commit

---
+++ DOMgetElementById.js
@@ -0,0 +1,10 @@
+const heading = document.getElementById("main-heading");
+heading.textContent = "Fruit World";
+heading.style.color = "orange";
+const header = document.getElementById("header");
+header.style.backgroundColor = "green";
+header.style.borderBottom = "3px solid orange";
+const basket = document.getElementById("basket-heading");
+basket.style.color = "green";
+const thank = document.getElementById("thanks");
+thank.innerHTML = "<p>Please visit us again</p>";
